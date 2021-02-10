7c50c821e18492525ba8491a67e5e5b50763bbda
madhuri bhandari <madhuri.bhandari1995@gmail.com>
2021-02-10 12:22:56 +0530

    modified file

--- check.php
+++ check.php
@@ -3,24 +3,24 @@
 include_once('db.php');
 
 
-// if(isset($_POST['email']) && isset($_POST['password']))
-// {
+if(isset($_POST['email']) && isset($_POST['password']))
+{
 
-//   $email = $_POST['email'];
-//   $password = $_POST['password'];
+  $email = $_POST['email'];
+  $password = $_POST['password'];
 
-//   $query = "select * from user where email='$email' and password='$password'";
-//   $result=mysqli_query($conn,$query);
+  $query = "select email,password from user where email='$email' and password='$password'";
+  $result=mysqli_query($conn,$query);
   
-//    if($result->num_rows==1)
-//    {
-//     echo 1;
-//    }
-//    else
-//    {
-//     echo 0;
+   if($result->num_rows==1)
+   {
+    echo 1;
+   }
+   else
+   {
+    echo 0;
    
-//    }
-// }
+   }
+}
 
 ?>
