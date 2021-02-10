c03bc229d1b6da9fbb3e0e34b95ed3d9bf9d2dbb
madhuri bhandari <madhuri.bhandari1995@gmail.com>
2021-02-10 17:16:05 +0530

    modified file

--- check.php
+++ check.php
@@ -1,11 +1,11 @@
 <?php
 
-include_once('db.php');
+//include_once('db.php');
 if(isset($_POST['email']) && isset($_POST['password']))
 {
 
-  $email = $_POST.get['email'];
-  $password = $_POST.get['password'];
+  $email = $_POST['email'];
+  $password = $_POST['password'];
 
   $query = "select * from user where email='$email' and password='$password'";
   $result=mysqli_query($conn,$query);
