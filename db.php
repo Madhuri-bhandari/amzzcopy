6187c71a05605e405adcac09a8fc3f7987d6316a
madhuri bhandari <madhuri.bhandari1995@gmail.com>
2021-02-10 12:28:56 +0530

    modified file

--- db.php
+++ db.php
@@ -1,9 +1,9 @@
 <?php
 
 $conn=new mysqli('localhost','root','','zennix');
-if($conn)
- // echo "connect";
-else
- // echo "not connect";
+// if($conn)
+//  echo "connect";
+// else
+//  echo "not connect";
 
 ?>
