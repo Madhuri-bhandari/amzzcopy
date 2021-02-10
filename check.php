4cb544d0e3779743f98eec234ae79ea67cce0e83
madhuri bhandari <madhuri.bhandari1995@gmail.com>
2021-02-10 17:25:36 +0530

    modified file

--- check.php
+++ check.php
@@ -7,10 +7,11 @@ if(isset($_POST['email']) && isset($_POST['password']))
   $email = $_POST['email'];
   $password = $_POST['password'];
 
-  $query = "select * from user where email='$email' and password='$password'";
-  $result=mysqli_query($conn,$query);
+  // $query = "select * from user where email='$email' and password='$password'";
+  // $result=mysqli_query($conn,$query);
   
-   if($result->num_rows==1)
+   //if($result->num_rows==1)
+  if($email=="madhuri@gmail.com"&& $password=="12345")
    {
     echo 1;
    }
